1691866a50f162673c1999d9ff2b9fc2fe9ca4d3
Francisco Rangel <francorangelcoronado@gmail.com>
2024-04-20 11:52:14 -0600

    aprendiendo rutas en flask

--- main.py
+++ main.py
@@ -5,5 +5,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+@app.route('/index')
+def index():
     return '<h1>Hello World!</h1>'
+
+
+@app.route('/hello')
+@app.route('/hello/<string:name>')
+@app.route('/hello/<string:name>/<int:age>')
+def hello(name=None, age=None):
+    if name is None and age is None:
+        return f'<h1>Hello my friend</h1>'
+    elif age is None:
+        return f'<h1>Hello {name}</h1>'
+    else:
+        return f'<h1>Hello {name} - {age}</h1>'
